e526c0d5e304b184858d14a046f99d8e5cbab25e
Arturas Molcanovas <a.molcanovas@gmail.com>
2020-04-25 16:44:59 +0100

    refactor: generate shorter style IDs

--- src/lib/makeStyleId.ts
+++ src/lib/makeStyleId.ts
@@ -1,23 +1,5 @@
-import {createHash} from 'crypto';
+const uniqueSlug = require('unique-slug'); //tslint:disable-line:no-var-requires
 
-const reg = /([=\/+])/g;
-//tslint:disable:object-literal-sort-keys
-const replacements = {
-  '/': '.',
-  '+': '-',
-  '=': '_'
-};
-//tslint:enable:object-literal-sort-keys
-
-/** Replace some base64 chars with valid ID characters */
-function makeStyleIdReplacer(m: string): string {
-  return replacements[m] || m;
-}
-
-/** @internal */
 export function makeStyleId(code: string): string {
-  return createHash('md5')
-    .update(code)
-    .digest('base64')
-    .replace(reg, makeStyleIdReplacer);
+  return uniqueSlug(code);
 }
